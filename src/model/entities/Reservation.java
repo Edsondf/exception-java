032ce302069e36526f7bd6a4da82256f2a1c8f93
Edsondf <edson.oliveira83@gmail.com>
2023-01-09 23:49:39 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer rooNumber;
@@ -12,8 +14,11 @@ public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation(Integer rooNumber, Date checkIn, Date checkOut) {
-		super();
+	public Reservation(Integer rooNumber, Date checkIn, Date checkOut)  {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		
 		this.rooNumber = rooNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -35,19 +40,18 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
 		if(!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
+			throw new DomainException("Check-out date must be after check-in date");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	@Override
 	public String toString() {
@@ -60,5 +64,7 @@ public class Reservation {
 				+ ", "
 				+ duration()
 				+ " nights";
+	
 	}
 }
+
